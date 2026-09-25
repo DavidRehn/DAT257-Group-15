794680ba10d3d987c0da9be7e902a56a7f948f45
Iinguss <darian.damcc@gmail.com>
2026-09-25 16:58:30 +0200

    descritinon for SaveInterface

--- src/main/java/saveload/SaveLoadInterface.java
+++ src/main/java/saveload/SaveLoadInterface.java
@@ -1,6 +1,9 @@
 package saveload;
 
 import java.util.Hashtable;
+ /** used by JsonSaveLoad as an implementation
+* @pram save values only accepts wraper typerna av Integer, Double & String
+*/
 public interface SaveLoadInterface {
     void save(Hashtable<String,Object> values);
     Hashtable<String,Object> load();
