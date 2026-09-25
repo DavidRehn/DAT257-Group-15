deeef9e42f1b3d335bf29decb1397d949ad0c717
Iinguss <darian.damcc@gmail.com>
2026-09-25 16:59:54 +0200

    bbb

--- src/main/java/saveload/SaveLoadInterface.java
+++ src/main/java/saveload/SaveLoadInterface.java
@@ -5,6 +5,7 @@ import java.util.Hashtable;
 * @pram save values only accepts wraper typerna av Integer, Double & String
 */
 public interface SaveLoadInterface {
-    void save(Hashtable<String,Object> values);
-    Hashtable<String,Object> load();
+    public void save(Hashtable<String,Object> values);
+    
+    public Hashtable<String,Object> load();
 }
